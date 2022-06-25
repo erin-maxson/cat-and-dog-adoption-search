70f7bb01844d0cf8f5093f81930b7dd7a50a0a64
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-06-24 22:41:02 -0400

    home page search now stores inital values for animal type and search location in local storage and redirects to animal search page to display results

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,1 +1,14 @@
 var submitBtnEl = document.getElementById('submitBtn')
+var animalTypeEl = document.getElementById('animal-type')
+var searchLocationEl = document.getElementById('findlocate')
+
+function animalSearchHandler(event){
+    event.preventDefault();
+
+     localStorage.setItem('initalAnimalType', animalTypeEl.value)
+     localStorage.setItem('initialSearchLocation', searchLocationEl.value)
+
+     document.location.href='./animal-search.html';
+}
+
+submitBtnEl.addEventListener('click', animalSearchHandler)
