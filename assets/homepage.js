d9417817ea04a0f235d96adc2c33c6294eb867a3
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-06-24 22:48:37 -0400

    added comments for readability to homepage.js

--- assets/homepage.js
+++ assets/homepage.js
@@ -2,13 +2,17 @@ var submitBtnEl = document.getElementById('submitBtn')
 var animalTypeEl = document.getElementById('animal-type')
 var searchLocationEl = document.getElementById('findlocate')
 
+// function for handling search selections and redirect to search results
 function animalSearchHandler(event){
     event.preventDefault();
 
+    // set user search preferences in local storage
      localStorage.setItem('initalAnimalType', animalTypeEl.value)
      localStorage.setItem('initialSearchLocation', searchLocationEl.value)
 
+     // redirect to search page to display results
      document.location.href='./animal-search.html';
 }
 
+// event listener for search submit
 submitBtnEl.addEventListener('click', animalSearchHandler)
